873495d990cd2cc27c1ba880f2f4da25be7540ab
Jay Holt <johnpholt3@gmail.com>
2022-08-11 13:03:52 -0400

    Updated background

--- src/components/Background.js
+++ src/components/Background.js
@@ -6,9 +6,9 @@ export default function Background() {
         <><div style={{ display: "flex", flexDirection: "row",}} /><div>
             <Card style={{ width: "1300px", minHeight: "428px", marginBottom: "50px", backgroundColor: "#DEEED6" }} className="text-black">
            
-            <Card.Img src="https://github.com/jholt3/RR-Bootstrap-Replica/blob/main/src/homepage-background.jpg" alt="Card image" />
+            <Card.Img src="https://d2d8wwwkmhfcva.cloudfront.net/1500x1520/d2guulkeunn7d8.cloudfront.net/assets/homepage/homepage-mobile-background-m3-high-res-ca3279087ea0a3337f671b694b196d43fd207443d5825e61295b0996974835e9.jpg" alt="Card image" />
 
-                <Card.ImgOverlay style={{ width: "578px", height: "428px", marginLeft: "100px", marginBottom: "50px",}}>
+                <Card.ImgOverlay style={{ top: "100px", width: "578px", height: "428px", marginLeft: "100px", marginBottom: "50px",}}>
                     <Card.Text style={{ fontSize: "2.5em", fontWeight: "bold" }}>
                         Order groceries for delivery <br /> or pickup today
                     </Card.Text>
