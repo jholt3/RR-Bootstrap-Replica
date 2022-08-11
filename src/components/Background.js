109e1f72f5e4c80940dc90d2cfa8cd816fd1eeb1
Jay Holt <johnpholt3@gmail.com>
2022-08-11 13:01:12 -0400

    Added Bootstrap styling

---
+++ src/components/Background.js
@@ -0,0 +1,36 @@
+import Card from 'react-bootstrap/Card'
+import Form from 'react-bootstrap/Form'
+
+export default function Background() {
+    return (
+        <><div style={{ display: "flex", flexDirection: "row",}} /><div>
+            <Card style={{ width: "1300px", minHeight: "428px", marginBottom: "50px", backgroundColor: "#DEEED6" }} className="text-black">
+           
+            <Card.Img src="https://github.com/jholt3/RR-Bootstrap-Replica/blob/main/src/homepage-background.jpg" alt="Card image" />
+
+                <Card.ImgOverlay style={{ width: "578px", height: "428px", marginLeft: "100px", marginBottom: "50px",}}>
+                    <Card.Text style={{ fontSize: "2.5em", fontWeight: "bold" }}>
+                        Order groceries for delivery <br /> or pickup today
+                    </Card.Text>
+                    <Card.Text style={{ fontSize: "1em" }}>
+                        Whatever you want from local stores, brought right to your door.
+                    </Card.Text>
+                    <Form>
+                        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                            <Form.Control size="lg" type="text" placeholder="   Enter your address                                         ->" />
+                        </Form.Group>
+                    </Form>
+                </Card.ImgOverlay>
+            </Card>
+        </div>
+      
+        </>
+    )
+
+}
+
+    
+    
+
+       
+
